3c319527cc72ec68b0dcec7d9c89406bd7408722
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2022-01-22 09:20:54 +0700

    Menambah Content Home

--- website/models.py
+++ website/models.py
@@ -4,11 +4,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
--- website/views.py
+++ website/views.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, flash, jsonify
 from flask.helpers import flash
 from flask_login import login_required, current_user
 from sqlalchemy.sql.functions import user
-from .models import Note
 from . import db
 import json
 
@@ -10,16 +9,7 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-
-        if len(note) <= 1:
-            flash('Catatan terlalu pendek!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Catatan telah ditambahkan.', category='success')
+    
 
     return render_template('home.html', user=current_user) # Route Homepage
 
