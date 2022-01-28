d623f594eeb737f9491356660e397905c7310b4a
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2022-01-28 14:57:58 +0700

    Menghapus kolom password pada form update

--- website/views.py
+++ website/views.py
@@ -139,24 +139,17 @@ def update():
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
 
         user = User.query.filter_by(email=email).first()
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
         else:
             try:
                 print(user)
                 user.first_name = first_name
                 user.email = email
-                user.password = generate_password_hash(password1, method='sha256')
                 db.session.commit()
             except Exception as e:
                 flash('Gagal Mengupdate Data', category='error')
